6e3307b2786136f648d0c3d892358ec5ef1c2cd6
Peter-Sciberras <sciberrasp@yahoo.com.au>
2023-01-16 19:27:48 +1100

    done with assignment

---
+++ index.js
@@ -0,0 +1,14 @@
+// Function Expression 
+const divide = () => {
+    return 2000 / 100;
+  }
+  
+  // Arrow Function
+  const square = (num) => {
+    return num * num;
+  }
+  
+  // Arrow Function
+  const add = (param1, param2) => {
+    return param1 + param2;
+  }
